b6c11d469cefa88902ffa21b669ef021331517fb
Stella Mettifogo <stellamettifogo@gmail.com>
2019-06-24 16:11:14 +0200

    refactored my tests code

--- stock.py
+++ stock.py
@@ -10,8 +10,8 @@ def best_buy(prices):
         # loop interno (sell)
         for j in range(i +1, prices_len):
             # if meglio, segna
-            best_so_far = series[sell] - series[buy]
-            best_now = series[j] - series[i]
+            best_so_far = prices[sell] - prices[buy]
+            best_now = prices[j] - prices[i]
             if best_now > best_so_far:
                 buy = i 
                 sell = j
@@ -20,35 +20,34 @@ def best_buy(prices):
 
 
 
-# test series
-series = [1, 2, 3, 4, 5]
-buy1, sell1 = best_buy(series)
-assert buy1 == 0 
-assert sell1 == 4
-# this string is formatted and tells the best time to buy and sell plus total gain
-print(f"buy[{buy1}]: ${series[buy1]}, sell[{sell1}]: ${series[sell1]}, gain: ${series[sell1]-series[buy1]}")
+datasets = [
+    {
+        "series": [5, 6, 7, 5, 4, 3, 2, 3, 4, 5, 6, 5, 4, 2, 0],
+        "buy": 6,
+        "sell": 10
+    },
+    {
+        "series": [1, 2, 3, 0, 1, 5, 7, 8, 6, 2],
+        "buy": 3,
+        "sell": 7
+    },
+    {
+        "series": [1, 2, 3, 4, 5, 3, 0],
+        "buy": 0,
+        "sell": 4
+    },
+    {
+        "series": [1, 2, 3, 4, 5],
+        "buy": 0,
+        "sell": 4
+    }
+]
 
-# test series
-series = [1, 2, 3, 4, 5, 3, 0]
-buy, sell = best_buy(series)
-assert buy == 0 
-assert sell == 4
-print(f"buy[{buy}]: ${series[buy]}, sell[{sell}]: ${series[sell]}, gain: ${series[sell]-series[buy]}")
-
-# test series
-series = [1, 2, 3, 0, 1, 5, 7, 8, 6, 2]
-buy, sell = best_buy(series)
-assert buy == 3 
-assert sell == 7
-print(f"buy[{buy}]: ${series[buy]}, sell[{sell}]: ${series[sell]}, gain: ${series[sell]-series[buy]}")
-
-
-
-# regression testing
-series = [5, 6, 7, 5, 4, 3, 2, 3, 4, 5, 6, 5, 4, 2, 0]
-buy, sell = best_buy(series)
-assert buy == 6 
-assert sell == 10
-
-print(f"buy[{buy}]: ${series[buy]}, sell[{sell}]: ${series[sell]}, gain: ${series[sell]-series[buy]}")
-
+for data in datasets:
+    rbuy, rsell = best_buy(data["series"])
+    assert rbuy == data["buy"]
+    assert rsell == data["sell"]
+    print(f"series: {data['series']}")
+    print(f"buy[{rbuy}]: ${data['series'][rbuy]}")
+    print(f"sell[{rsell}]: ${data['series'][rsell]}")
+    print(f"gain: ${data['series'][rsell] - data['series'][rbuy]}\n\n")
